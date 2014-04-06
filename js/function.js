1051852a5581b6f6ee339b868d3a79775466dd94
Harrune <radek.s87@gmail.com>
2014-04-06 17:28:23 +0200

    add slider, one photo for test and bunch of styles.

---
+++ js/function.js
@@ -0,0 +1,12 @@
+/*** All scripts go here ****/
+
+
+$(function(){
+    
+   $('#main').find('.bxslider').bxSlider({
+       mode: 'fade',
+       captions: true,
+       auto: true
+   });
+    
+});
